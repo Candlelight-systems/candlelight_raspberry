9734358ed75769e46c2c92846f303e4ae217b1ee
Norman Pellet <norman.pellet@gmail.com>
2017-11-15 08:25:58 +0000

    Add socket pings

--- wsconnection.js
+++ wsconnection.js
@@ -3,6 +3,24 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server( { port: 8081 } );
 
+wss.on('connection', function connection( ws ) {
+  ws.isAlive = true;
+  ws.on('pong', () => ws.isAlive = true );
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach( ( ws ) => {
+
+    if (ws.isAlive === false) {
+     return ws.terminate();
+ 	}
+
+    ws.isAlive = false;
+    ws.ping( '', false, true );
+
+  } );
+}, 30000 );
+
 module.exports = {
 
 	send: ( data ) => {
